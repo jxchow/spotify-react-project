a8e7a59c0a17c089746a0147453954ab781e6b88
Sheryl Chang <sheryl.chang@berkeley.edu>
2019-04-30 18:47:57 -0700

    finished currently playing view

--- src/components/CurrentlyPlaying.jsx
+++ src/components/CurrentlyPlaying.jsx
@@ -3,6 +3,16 @@ import '../styles/CurrentlyPlaying.css';
 
 export default class CurrentlyPlaying extends React.Component {
     render () {
-        return null;
+        return (
+        	<div className="currently-playing-container">
+        		<img src="http://placekitten.com/400/300"></img>
+      			<div className="currently-playing-title">
+      				<h1>Song Title</h1>
+      			</div>
+      			<div className="currently-playing-artist">
+      				Song Artist
+      			</div>
+    		</div>
+        );
     };
 }
--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -4,5 +4,7 @@ import '../styles/MusicPlayer.css';
 export default class MusicPlayer extends React.Component {
     render () {
         return null;
+        // has to manage state
+        // figure out what next button should do
     };
 }
